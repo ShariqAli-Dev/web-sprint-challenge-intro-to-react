da06951cc2505759186a7925ecb90f938d7b6f23
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-06-27 15:21:54 -0400

    Refactored Theme.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./App.css";
+// import "./App.css";
 import styled from "styled-components";
 import Characters from "./components/Characters";
 
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,4 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
-  primaryColor: "royalblue",
-  secondaryColor: "crimson",
+  app: {
+    textAlignment: "center",
+    width: "80vw",
+    margin: "auto",
+  },
+
+  title: {
+    primaryColor: "rgb(243, 157, 59)",
+    shadowColor: "1px 1px 5px rgb(30, 62, 136)",
+    fontSize: "4rem",
+  },
+
+  halfBlack: {
+    backgroundColor: "rgba(14, 11, 11, 0.2)",
+    width: "60vw",
+    margin: "auto",
+  },
+
+  characterDiv: {
+    fontSize: "2rem",
+    margin: "5%",
+    padding: "2.5%",
+    border: "solid rgb(57, 19, 119) 2px",
+    color: "rgb(239, 179, 241)",
+  },
 };
